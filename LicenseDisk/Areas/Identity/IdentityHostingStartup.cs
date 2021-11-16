2023aae8abd74e31e9db37bbaa39752b557ec573
l224 <l224@L224D33578>
2021-11-16 14:55:34 +0200

    Add project files.

---
+++ LicenseDisk/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using LicenseDisk.Areas.Identity.Data;
+using LicenseDisk.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LicenseDisk.Areas.Identity.IdentityHostingStartup))]
+namespace LicenseDisk.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<LicenseDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("LicenseDbContextConnection")));
+
+                services.AddDefaultIdentity<LicenseUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    })
+                    .AddEntityFrameworkStores<LicenseDbContext>();
+            });
+        }
+    }
+}
---
+++ LicenseDisk/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LicenseDisk.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
